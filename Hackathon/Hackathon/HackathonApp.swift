6222b4caa3edd02a06b291a995b7b97623df40ac
Ricardo Dórame <rhdorame@gmail.com>
2024-02-23 13:05:04 -0600

    Initial commit and architecture

---
+++ Hackathon/Hackathon/HackathonApp.swift
@@ -0,0 +1,17 @@
+//
+//  HackathonApp.swift
+//  Hackathon
+//
+//  Created by Ricardo Dórame on 23/02/24.
+//
+
+import SwiftUI
+
+@main
+struct HackathonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
